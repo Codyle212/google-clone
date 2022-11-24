e33db2ebb0d46046c4b876cf14d7a62fd672d858
Shi Ning Le <shining@ualberta.ca>
2022-11-24 14:48:32 -0700

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = (): JSX.Element => {
     return (
         <footer className="fixed bottom-0 left-[50%] translate-x-[-50%] whitespace-nowrap p-6 text-gray-600 bg-[#f8f9fa] min-w-full min-height-[50%] ">
-            <div className="flex flex-col sm:flex-row justify-between  items-center text-xs ">
+            <div className="flex flex-col space-y-4 sm:flex-row sm:space-y-0 justify-between  items-center text-xs ">
                 <div className="flex space-x-8 mx-5 sm:ml=0 ">
                     <a href="https://ads.google.com/">
                         <p className="link">Advertising</p>
