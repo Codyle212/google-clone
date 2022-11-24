212bab192595901afd2f80677d1c3711546c8fa2
Shi Ning Le <shining@ualberta.ca>
2022-11-23 23:56:35 -0700

    added body styling

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,8 @@
 import Head from 'next/head';
+import Image from 'next/image';
 import Header from '../components/Header';
+import googleLogo from '../public/google_logo.svg';
+import { MagnifyingGlassIcon, MicrophoneIcon } from '@heroicons/react/20/solid';
 
 export default function Home() {
     return (
@@ -12,9 +15,30 @@ export default function Home() {
                 />
                 <link rel="icon" href="/favicon.ico" />
             </Head>
-            <Header />
             {/*Header*/}
+            <Header />
             {/*Body*/}
+            <form className="flex flex-col items-center mt-40">
+                <Image
+                    src={googleLogo}
+                    objectFit="cover"
+                    alt="google logo"
+                    width={300}
+                    height={100}
+                />
+                <div className="flex w-full mt-5 mx-auto max-width-[90%] border border-gray-200 hover:shadow-lg focus-within:shadow-lg px-5 py-3 rounded-full items-center sm:max-w-xl lg:max-w-2xl">
+                    <MagnifyingGlassIcon className="h-5 text-gray-500 mr-3" />
+                    <input
+                        type="text"
+                        className="flex-grow focus:outline-none"
+                    />
+                    <MicrophoneIcon className="h-5" />
+                </div>
+                <div className="flex flex-col sm:flex-row w-[50%] space-y-2 mt-8 sm:space-y-0 sm:space-x-4 justify-center">
+                    <button className="btn">Google Search</button>
+                    <button className="btn">{`I'm Feeling Lucky`}</button>
+                </div>
+            </form>
             {/*Footer*/}
         </div>
     );
